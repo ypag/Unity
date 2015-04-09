505b253ff0060d24fcad246ef44bd001072437e2
ypag <ypag@users.noreply.github.com>
2015-04-09 14:03:04 -0400

    Cleaned up the UI
    , printing time period in center of screen

--- PendulumTrack.cs
+++ PendulumTrack.cs
@@ -1,8 +1,6 @@
 // This script has to be attached to image target that we want to track. 
 
-
-using UnityEngine;
-using System.Collections;
+sing System.Collections;
 
 public class TargetImageCoords : MonoBehaviour {
 
@@ -16,6 +14,7 @@ public class TargetImageCoords : MonoBehaviour {
 	private float oldTime = 0f; 
 	public float period = 0f;
 	private float newXAbs = 0f;
+	public float midpointX = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -28,71 +27,86 @@ public class TargetImageCoords : MonoBehaviour {
 		{
 			Debug.Log ("ImageTargetBehaviour not found ");
 		}
-		// adding co-routine here so that it doesn't get called multiple times, do not put this in update function
 		StartCoroutine (TimeKeep());
 
 	}
 
 
-	// Update is called once per frame 
-	//FixedUpdate makes sure that the loop takes fixed amount of time to run, this is to make sure that we get
-	//reliable period value every time fixedupdate runs. 
-	void FixedUpdate () {	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{	
 				if (mImageTargetBehaviour == null) {
 						Debug.Log ("ImageTargetBehaviour not found");
 						return;
-				}
+				} 
+
+						Vector2 targetSize = mImageTargetBehaviour.GetSize ();
+						float targetAspect = targetSize.x / targetSize.y;
 		
-				Vector2 targetSize = mImageTargetBehaviour.GetSize ();
-				float targetAspect = targetSize.x / targetSize.y;
+						// We define a point in the target local reference 
+						// we take the bottom-left corner of the target, 
+						// just as an example
+						// Note: the target reference plane in Unity is X-Z, 
+						// while Y is the normal direction to the target plane
+						Vector3 pointOnTarget = new Vector3 (-0.5f, 0, -0.5f / targetAspect); 
 		
-				// We define a point in the target local reference 
-				// we take the bottom-left corner of the target, 
-				// just as an example
-				// Note: the target reference plane in Unity is X-Z, 
-				// while Y is the normal direction to the target plane
-				Vector3 pointOnTarget = new Vector3 (-0.5f, 0, -0.5f / targetAspect); 
+						// We convert the local point to world coordinates
+						Vector3 targetPointInWorldRef = transform.TransformPoint (pointOnTarget);
 		
-				// We convert the local point to world coordinates
-				Vector3 targetPointInWorldRef = transform.TransformPoint (pointOnTarget);
-		
-				// We project the world coordinates to screen coords (pixels)
-				Vector3 screenPoint = Camera.main.WorldToScreenPoint (targetPointInWorldRef);
-		
-				//Debug.Log ("target point in screen coords: " + screenPoint.x + ", " + screenPoint.y);
-				newXAbs = XAbs;
-				XAbs = Mathf.Abs (screenPoint.x);
-				YAbs = Mathf.Abs (screenPoint.y);
-				if (XAbs > newXAbs) {
-						TimeKeep ();
-				}
+						// We project the world coordinates to screen coords (pixels)
+						Vector3 screenPoint = Camera.main.WorldToScreenPoint (targetPointInWorldRef);
+
+						newXAbs = XAbs;
+						XAbs = Mathf.Abs (screenPoint.x);
+						YAbs = Mathf.Abs (screenPoint.y);
+				
 				
 
-				//Debug.Log("Abs Value is:" + XAbs + "," + YAbs);
-        // Calculate maximum X
-				XMax = Mathf.Max (XAbs, XMax);
+						//Debug.Log("Abs Value is:" + XAbs + "," + YAbs);
 
-				XMin = Mathf.Min (XAbs, XMin);
-				Vector3 ScreenPointMax = new Vector3 (XMax, 0f, YMax);
-				YMax = Mathf.Max (YAbs, YMax);
+						XMax = Mathf.Max (XAbs, XMax);
+						//midpointX = Screen.width / 2;
 
-		}
+
+				
+						XMin = Mathf.Min (XAbs, XMin);
+						Vector3 ScreenPointMax = new Vector3 (XMax, 0f, YMax);
+						YMax = Mathf.Max (YAbs, YMax);
+
+				
+				
+
+	
+						//Debug.Log ("Maximum between x and y " + XMax + "," + YMax + " Min X is: " + XMin);
+						//GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+						//sphere.transform.position = new Vector3 (XMax, 1F, YMax);
+						//sphere.transform.localScale = new Vector3(
+		
+				
+	}
 
 	void OnGUI()
 	{
-	// outputting the variables on screen 
-		GUI.backgroundColor = Color.blue;
+	
+		GUIStyle style = new GUIStyle ();
+		style.fontSize = 30;
+		GUI.color = Color.green;
+		//style.font.material.color = Color.green;
+		//GUI.contentColor = Color.green;
+
+	
 		GUI.Label(new Rect(10, 70, 100, 80), "Maximum X:" +XMax.ToString());
 		GUI.Label (new Rect (10, 100, 100, 80), "Minimum X:" + XMin.ToString ());
 		GUI.Label (new Rect (10, 130, 100, 80), "Maximum Y:" + YMax.ToString ());
 		GUI.Label (new Rect (10, 180, 100, 80), "timestart: " + newTime.ToString ());
-		GUI.Label (new Rect (10, 220, 100, 80), "Time Period " + period.ToString ());
+		GUI.Box (new Rect (100, 70, 100, 80), "Time Period:  " + period.ToString (), style);
 		GUI.Label (new Rect (10, 250, 100, 80), "oldtime: " + oldTime.ToString ());
 		GUI.Label (new Rect (10, 10, 100, 80), "Current Position X:" + XAbs.ToString ());
 		GUI.Label (new Rect (10, 40, 100, 80), "Current Position Y:" + YAbs.ToString ());
 
 
 	}
+	//float x = 0f;
 
 	IEnumerator TimeKeep()
 	{
@@ -103,16 +117,18 @@ public class TargetImageCoords : MonoBehaviour {
 			Debug.Log (" NEW: " + newTime);
 			//Debug.Log ("old period:" + period);
 			period = (newTime - oldTime); 
+			//XMax = 0f; 
 				
 
 		//Debug.Log ("new period:" + period);
 
 	
 		}
-		//delay is important to reject noisy values around XMax
-		yield return new WaitForSeconds (2F);
-		//To call start co routine again, hence timekeep() is looped. 
+
+		yield return new WaitForSeconds (1F);
 		StartCoroutine (TimeKeep());
+		//XMax = Mathf.NegativeInfinity;
+
 	}
 	
 
